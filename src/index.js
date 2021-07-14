f8df4c4b4c06bda1be21ca474952612292f08e06
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-07-14 19:04:04 +0100

    main folder structure and src index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const { connect } = require("./db");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+connect();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
