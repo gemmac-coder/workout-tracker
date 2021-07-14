1142c345aafa6a4973b090625317edc5757fcbec
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-07-14 20:03:08 +0100

    added workout model

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,41 @@
 const Workout = require("./Workout");
 
-module.exports = {
-  Workout,
+const Schema = mongoose.Schema;
+
+const schema = {
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 };
+const workoutSchema = new Schema(schema);
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
