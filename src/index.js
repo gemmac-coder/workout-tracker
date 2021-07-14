895cd0376223ada7a9473ee255266850477cd9eb
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-07-14 19:15:18 +0100

    config, db, and index.js in

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const path = require("path");
 
 const { connect } = require("./db");
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+
 const app = express();
+
 connect();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
 app.use(routes);
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
