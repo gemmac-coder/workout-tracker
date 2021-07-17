57e9f14a91d4d5e3817bffac0373e85696e63c52
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-07-17 11:00:41 +0100

    updated seed script for atlas

--- src/seeders/seed.js
+++ src/seeders/seed.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(DB_URL, MONGOOSE_OPTIONS);
 
 const workoutSeed = [
   {
